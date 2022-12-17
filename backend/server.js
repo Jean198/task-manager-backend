f370218ccc8c5fcf133c03722de50af7e09b699d
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-17 10:58:06 +0200

    changed the cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 //Middleware
 app.use(express.json()); //This is enough when sending data only as json
 app.use(express.urlencoded({ extended: false })); //When sending data as Form-data in postman
-app.use(cors());
+app.use(cors(
+  {
+origin:["http://local:3000", "https://task-manager-app.onrender.com"]
+}
+));
 app.use("/api/tasks", taskRoutes);
 
 
